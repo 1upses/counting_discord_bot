83f6c42daaeda409de8d146c03d17aa9df5454ca
1ups_ <61560124+1upses@users.noreply.github.com>
2023-02-01 15:45:52 +0100

    bug fix

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ async def on_ready(): print('cum')
 @client.event
 async def on_message(message):
     message_content: str = str(message.content)
+    channel = message.channel.id
+    if channel != CHANNEL_ID: return
     try:
         int(message_content)
     except ValueError:
         await message.delete()
         return
-    channel = message.channel.id
-    if channel != CHANNEL_ID: return
     if int(message_content) - 1 == get_number("data") and str(message.author) != get_user("data"):
         update_data("data", get_number("data"), str(message.author))
     else:
